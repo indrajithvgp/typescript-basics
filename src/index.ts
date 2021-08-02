a33bce70d8744ee4bad7cd57368e627077f1e896
Indrajith V <indrajithvgp@yahoo.com>
2021-08-02 14:45:10 +0530

    user, company class added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi from typescript")
+import {CustomMap} from './CustomMap'
+import {User} from './User'
+import {Company} from './Company'
+
+const user = new User()
+const company = new Company()
+
+const map = new CustomMap('map')
+
+map.addUserMarker(user)
+map.addCompanyMarker(company)
+
